2052149fac99b6b2c4dd2e7c95964a1976082e23
Lucas Briese <lbriese@mail.uni-paderborn.de>
2019-02-12 00:10:43 +0100

    ~ travis debugging, maybe to less RAM?

--- test/testHealth.sh
+++ test/testHealth.sh
@@ -18,22 +18,22 @@ fi
 
 echo "starting container Healthy, Healthy->Unhealthy, Unhealthy"
 docker run -d --rm --name "$NAME_HEALTHY" -e JAVA_PARAMETERS="-Xms1G -Xmx1G" "$IMAGE"
-docker run -d --rm --name "$NAME_UNHEALTHY" -e JAVA_PARAMETERS="-Xms1G -Xmx1G" -e HEALTH_PORT="20" "$IMAGE"
-docker run -d --rm --name "$NAME_UNHEALTHY2" -e JAVA_PARAMETERS="-Xms1G -Xmx1G" "$IMAGE"
+#docker run -d --rm --name "$NAME_UNHEALTHY" -e JAVA_PARAMETERS="-Xms1G -Xmx1G" -e HEALTH_PORT="20" "$IMAGE"
+#docker run -d --rm --name "$NAME_UNHEALTHY2" -e JAVA_PARAMETERS="-Xms1G -Xmx1G" "$IMAGE"
 
 if [ "$MODE" == 1 ]; then
 	exit 100
 fi
 
-# make unhealth
-docker cp $NAME_UNHEALTHY2:/home/checkHealth.sh MyFile
-if [ ! -e MyFile ]; then
-	echo "failed to copy checkHealth.sh"
-	exit 1
-fi
-echo "newLine" >> MyFile
-chmod a=rwx MyFile
-docker cp MyFile $NAME_UNHEALTHY2:/home/checkHealth.sh
+## make unhealth
+#docker cp $NAME_UNHEALTHY2:/home/checkHealth.sh MyFile
+#if [ ! -e MyFile ]; then
+#	echo "failed to copy checkHealth.sh"
+#	exit 1
+#fi
+#echo "newLine" >> MyFile
+#chmod a=rwx MyFile
+#docker cp MyFile $NAME_UNHEALTHY2:/home/checkHealth.sh
 
 sleep 100s
 if [ "$MODE" == 2 ]; then
@@ -63,37 +63,37 @@ else
 	echo "[INFO] Healthy container looks healthy"
 fi
 
-info=$(docker ps | grep -F -e "$NAME_UNHEALTHY")
-if [ $(echo "$info" | wc -c) == "0" ]; then
-	echo "[FATAL] couldn't run $NAME_UNHEALTHY"
-	docker ps
-	exit 4
-elif [ $(echo "$info" | grep -F -e unhealthy | wc -c) == "0" ]; then
-	echo "[ERROR] unhealth check failed"
-	docker ps
-	docker exec $NAME_UNHEALTHY /home/checkHealth.sh debugMode || true
-	echo "$info"
-	exit 5
-else
-	echo "[INFO] Unhealthy container looks Unhealthy"
-fi
+#info=$(docker ps | grep -F -e "$NAME_UNHEALTHY")
+#if [ $(echo "$info" | wc -c) == "0" ]; then
+#	echo "[FATAL] couldn't run $NAME_UNHEALTHY"
+#	docker ps
+#	exit 4
+#elif [ $(echo "$info" | grep -F -e unhealthy | wc -c) == "0" ]; then
+#	echo "[ERROR] unhealth check failed"
+#	docker ps
+#	docker exec $NAME_UNHEALTHY /home/checkHealth.sh debugMode || true
+#	echo "$info"
+#	exit 5
+#else
+#	echo "[INFO] Unhealthy container looks Unhealthy"
+#fi
+#
+#info=$(docker ps | grep -F -e "$NAME_UNHEALTHY2" )
+#if [ $(echo "$info" | wc -c) == "0" ]; then
+#	echo "[FATAL] couldn't run $NAME_UNHEALTHY2"
+#	docker ps
+#	exit 6
+#elif [ $(echo "$info" | grep -F -e unhealthy | wc -c) == "0" ]; then
+#	echo "[ERROR] unhealth check failed"
+#	docker ps
+#	docker exec $NAME_UNHEALTHY2 /home/checkHealth.sh debugMode || true
+#	echo "$info"
+#	exit 7
+#else
+#	echo "[INFO] Healthy Container successfully unhealthy."
+#fi
 
-info=$(docker ps | grep -F -e "$NAME_UNHEALTHY2" )
-if [ $(echo "$info" | wc -c) == "0" ]; then
-	echo "[FATAL] couldn't run $NAME_UNHEALTHY2"
-	docker ps
-	exit 6
-elif [ $(echo "$info" | grep -F -e unhealthy | wc -c) == "0" ]; then
-	echo "[ERROR] unhealth check failed"
-	docker ps
-	docker exec $NAME_UNHEALTHY2 /home/checkHealth.sh debugMode || true
-	echo "$info"
-	exit 7
-else
-	echo "[INFO] Healthy Container successfully unhealthy."
-fi
-
-docker stop "$NAME_HEALTHY"
-docker stop "$NAME_UNHEALTHY"
-docker stop "$NAME_UNHEALTHY2"
+docker stop "$NAME_HEALTHY" || true
+docker stop "$NAME_UNHEALTHY" || true
+docker stop "$NAME_UNHEALTHY2" || true
 exit 0
