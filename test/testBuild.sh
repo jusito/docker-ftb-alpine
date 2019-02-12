b170dde51b81f925b58638c706e5398363870715
Lucas Briese <lbriese@mail.uni-paderborn.de>
2019-02-12 20:56:35 +0100

    =fix, Travis vs Local

--- test/testBuild.sh
+++ test/testBuild.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-set -e
+set -o errexit
+set -o nounset
+set -o pipefail
 
 echo "build FTBBase"
 docker build -t "jusito/docker-ftb-alpine:FTBBase" .
@@ -8,6 +10,6 @@ docker build -t "jusito/docker-ftb-alpine:FTBBase" .
 for tag in $(ls modpacks)
 do
 	echo "[testBuild][INFO]build modpacks/$tag"
-	docker rmi "jusito/docker-ftb-alpine:$tag"
+	docker rmi "jusito/docker-ftb-alpine:$tag" || true
 	docker build -t "jusito/docker-ftb-alpine:$tag" "modpacks/$tag"
 done
--- test/testRun.sh
+++ test/testRun.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-set -e
+set -o errexit
+set -o nounset
+set -o pipefail
 
 # Travis debugging
 for tag in $(ls modpacks)
 do
 	echo "[testRun][INFO]running $tag"
-	docker run -ti --rm -e TEST_MODE=true -e JAVA_PARAMETERS="-Xms1G -Xmx2G" "jusito/docker-ftb-alpine:$tag"
-	ec=$?
-	if [ $ec != 0 ]; then
+	if ! docker run -ti --rm -e TEST_MODE=true -e JAVA_PARAMETERS="-Xms1G -Xmx2G" "jusito/docker-ftb-alpine:$tag"; then
 		exit $ec
 	fi
 done
