96e1de80ce7c76979e7462a8dd52426d18f4e399
Lucas Briese <lbriese@mail.uni-paderborn.de>
2019-09-28 14:02:30 +0200

    + added RLCraft 2.6.3 (non FTB)

--- modpacks/RLCraft/2.6.3-1.12.2/ServerStart.sh
+++ modpacks/RLCraft/2.6.3-1.12.2/ServerStart.sh
@@ -10,12 +10,9 @@ set -o nounset
 
 MCVER="1.12.2"
 MCJAR="minecraft_server.${MCVER}.jar"
-FORGEVER="14.23.5.2847"
-FORGEINSTALLER="forge-${MCVER}-${FORGEVER}-installer.jar"
-FORGEJAR="forge-${MCVER}-${FORGEVER}-universal.jar"
+FORGEINSTALLER="forge-${MCVER}-${FORGE_VERSION}-installer.jar"
+FORGEJAR="forge-${MCVER}-${FORGE_VERSION}-universal.jar"
 JAVACMD="java"
-
-"forge-1.12.2-14.23.5.2847-installer.jar"
 
 if [ -f "reinstall" ]; then
 	rm -f "reinstall"
@@ -24,18 +21,21 @@ else
 	REINSTALL=false
 fi
 
-if [ ! -f "${MY_VOLUME}/mods" ]; then
+if [ -f "${MY_VOLUME}"/RLCraft* ]; then
 	echo "[ServerStart]Needing to move server files"
-	mv -f "${MY_VOLUME}/RLCraft*/*" "${MY_VOLUME}"
+	mv -f "${MY_VOLUME}"/RLCraft*/* "${MY_VOLUME}"
+	rm -rf "${MY_VOLUME}"/RLCraft*
 fi
 
-if [ ! -f "${MY_VOLUME}/${MCJAR}" ] || [ "$REINSTALL" == "true " ]; then
+if [ ! -f "${MY_VOLUME}/${MCJAR}" ] || [ "$REINSTALL" = "true " ]; then
+	echo "[ServerStart]Downloading Minecraft Server"
 	wget -O "${MY_VOLUME}/${MCJAR}" "https://s3.amazonaws.com/Minecraft.Download/versions/${MCVER}/${MCJAR}"
 fi
-if [ ! -f "${MY_VOLUME}/${FORGEJAR}" ] || [ "$REINSTALL" == "true " ]; then
-	wget -O "${MY_VOLUME}/$FORGEJAR" "http://files.minecraftforge.net/maven/net/minecraftforge/forge/${MCVER}-${FORGEVER}/${FORGEINSTALLER}"
-	java -jar "${MY_VOLUME}/$FORGEJAR" --installServer
+if [ ! -f "${MY_VOLUME}/${FORGEJAR}" ] || [ "$REINSTALL" = "true " ]; then
+	echo "[ServerStart]Downloading Forge Server"
+	wget -O "${MY_VOLUME}/$FORGEINSTALLER" "http://files.minecraftforge.net/maven/net/minecraftforge/forge/${MCVER}-${FORGE_VERSION}/${FORGEINSTALLER}"
+	java -jar "${MY_VOLUME}/$FORGEINSTALLER" --installServer
 	rm -f "${MY_VOLUME}/$FORGEINSTALLER"
 fi
 
-"$JAVACMD" -server DUMMY_REPLACED -jar "${MY_VOLUME}/${FORGEJAR}" nogui
+"$JAVACMD" -server -Xmx2048 JAVA_PARAMETERS -jar "${MY_VOLUME}/${FORGEJAR}" nogui
