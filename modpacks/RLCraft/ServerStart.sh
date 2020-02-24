f2657abfb7ae359455d33d86cc3aacec34fedfbe
Lucas Briese <lbriese@mail.uni-paderborn.de>
2020-02-24 03:22:09 +0100

    ~ moved RLCraft script (no duplicates) + RLCraft using new base

--- modpacks/RLCraft/ServerStart.sh
+++ modpacks/RLCraft/ServerStart.sh
@@ -3,9 +3,8 @@
 if [ "${DEBUGGING:?}" = "true" ]; then
 	set -o xtrace
 fi
-
 set -o errexit
-set -o pipefail
+#set -o pipefail
 set -o nounset
 
 MCVER="1.12.2"
@@ -21,18 +20,20 @@ else
 	REINSTALL=false
 fi
 
-if [ -f "${MY_VOLUME}"/RLCraft* ]; then
+if [ -f "${MY_VOLUME}/RLCraft Server Pack*" ]; then
 	echo "[ServerStart]Needing to move server files"
-	mv -f "${MY_VOLUME}"/RLCraft*/* "${MY_VOLUME}" || true
-	rm -rf "${MY_VOLUME}"/RLCraft*
+	mv -f "${MY_VOLUME}/RLCraft Server Pack*/*" "${MY_VOLUME}"
+	rm -rf "${MY_VOLUME}/RLC*"
 fi
 
 if [ ! -f "${MY_VOLUME}/${MCJAR}" ] || [ "$REINSTALL" = "true " ]; then
 	echo "[ServerStart]Downloading Minecraft Server"
+	rm -rf minecraft_server.* || true
 	wget -O "${MY_VOLUME}/${MCJAR}" "https://s3.amazonaws.com/Minecraft.Download/versions/${MCVER}/${MCJAR}"
 fi
 if [ ! -f "${MY_VOLUME}/${FORGEJAR}" ] || [ "$REINSTALL" = "true " ]; then
 	echo "[ServerStart]Downloading Forge Server"
+	rm -rf forge-* || true
 	wget -O "${MY_VOLUME}/$FORGEINSTALLER" "http://files.minecraftforge.net/maven/net/minecraftforge/forge/${MCVER}-${FORGE_VERSION}/${FORGEINSTALLER}"
 	java -jar "${MY_VOLUME}/$FORGEINSTALLER" --installServer
 	rm -f "${MY_VOLUME}/$FORGEINSTALLER"
