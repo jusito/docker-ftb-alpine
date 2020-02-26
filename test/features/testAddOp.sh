8a34e5ba1577d54e6960de87f86b4cfb9ebca02f
Lucas Briese <lbriese@mail.uni-paderborn.de>
2020-02-27 00:44:44 +0100

    = fix, test add op failed

--- test/features/testAddOp.sh
+++ test/features/testAddOp.sh
@@ -9,16 +9,30 @@ readonly admin2="Hey_Schnitte"
 readonly tag=$1
 echo "[testAddOp][INFO] starting tag=$tag"
 
+docker stop "$TEST_CONTAINER" || true
+docker rm "$TEST_CONTAINER" || true
 docker run -d \
  --name "$TEST_CONTAINER" \
  -e DEBUGGING="${DEBUGGING}" \
- -e TEST_MODE=true -e JAVA_PARAMETERS="-Xms1G -Xmx1G" \
+ -e JAVA_PARAMETERS="-Xms2G -Xmx2G" \
  -e ADMIN_NAME="$admin1" \
  "${REPO}:$tag"
+
+await "$TEST_CONTAINER" "/home/docker/logs/latest.log" "]: Done ("
+if docker exec "$TEST_CONTAINER" grep -q "$admin1" "/home/docker/ops.json"; then
+	echo "[testAddOp][INFO] found first op"
+else
+	echo "[testAddOp][ERROR] found no op"
+	exit 31
+fi
 
 docker exec "$TEST_CONTAINER" /home/addOp.sh "" "$admin2" "" ""
 
 docker restart "$TEST_CONTAINER"
+docker exec "$TEST_CONTAINER" rm /home/docker/logs/latest.log
+await "$TEST_CONTAINER" "/home/docker/logs/latest.log" "]: Done ("
+#docker exec "$TEST_CONTAINER" ls -lA /home #debug OUT
+#docker exec "$TEST_CONTAINER" ls -lA /home/docker #debug OUT
 if docker exec "$TEST_CONTAINER" grep -q "$admin1" "/home/docker/ops.json"; then
 	if docker exec "$TEST_CONTAINER" grep -q "$admin2" "/home/docker/ops.json"; then
 		echo "[testAddOp][INFO] found both ops"
