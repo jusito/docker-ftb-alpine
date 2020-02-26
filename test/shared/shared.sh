3535489b368dd40ebb712c1f44834f13b13425ca
Lucas Briese <lbriese@mail.uni-paderborn.de>
2020-02-27 00:42:52 +0100

    ~ moved await (for given log entry) to shared

--- test/shared/shared.sh
+++ test/shared/shared.sh
@@ -18,7 +18,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-loadBaseImages() {
+function loadBaseImages() {
 	if cd "base"; then
 		for javaVersion in *; do
 		if [ -d "$javaVersion" ] && cd "$javaVersion"; then
@@ -36,7 +36,7 @@ declare -a BASE_IMAGES
 loadBaseImages
 #echo "${BASE_IMAGES[*]}"
 
-loadModpackImages() {
+function loadModpackImages() {
 	if cd "modpacks"; then
 		for modpack in *; do
 		if [ -d "$modpack" ] && cd "$modpack"; then
@@ -51,12 +51,57 @@ declare -a MODPACKS
 loadModpackImages
 #echo "${MODPACKS[*]}"
 
-getImageTag() {
+function getImageTag() {
 	# shellcheck disable=SC2001
 	echo "$1" | sed 's/!/-/g'
 }
 
-getImagePath() {
+function getImagePath() {
 	# shellcheck disable=SC2001
 	echo "$1" | sed 's/!/\//g'
+}
+
+function await() {
+	container=$1
+	file=$2
+	waitFor=$3
+	#minTime=$((30))
+		
+	isRunning=true
+	counter=0
+	timeout=300
+	step=1
+	while [ $isRunning = true ]; do
+		counter=$((counter+1))
+		
+		if [ "$step" = "1" ]; then
+			echo -en "\r[shared.await][INFO] waiting[-]..."
+		elif [ "$step" = "2" ]; then
+			echo -en "\r[shared.await][INFO] waiting[\\]..."
+		else
+			echo -en "\r[shared.await][INFO] waiting[/]..."
+			step=0
+		fi
+		step=$((step+1))
+		
+		if docker exec "$container" grep -Fq -e "$waitFor" "$file" 2>/dev/null; then
+			isRunning=false
+			timeout=$((timeout+1))
+		elif [ $counter -ge $timeout ]; then
+			isRunning=false			
+		fi
+		
+		sleep 1s
+	done
+	
+	if [ $counter -ge $timeout ]; then
+		echo -en "\r[shared.await][ERROR] TIMEOUT! \n"
+		return 1
+	else
+		echo -en "\r[shared.await][INFO] await done\n"
+		#if [ $counter -lt $minTime ]; then
+		#	sleep $((minTime-counter))s
+		#fi
+		return 0
+	fi
 }
