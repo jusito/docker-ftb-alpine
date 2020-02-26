942673bb84ff6357aef082d5ad28357021f7989f
Lucas Briese <lbriese@mail.uni-paderborn.de>
2020-02-26 19:16:50 +0100

    + added simple server query script, entrypoint and below unfinished #1

---
+++ base/8jre/alpine/serverQuery.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+if [ "${DEBUGGING:?}" = "true" ]; then
+        set -o xtrace
+fi
+set -o errexit
+#set -o pipefail
+set -o nounset
+
+readonly USE_LOG="/home/docker/logs/latest.log"
+command="$*"
+
+
+
+
+getLogLines() {
+	wc -l "$USE_LOG" | grep -Eo '^\d+'
+}
+querySend() {
+    echo "$@" > "$SERVER_QUERY_PIPE"
+}
+
+
+
+
+
+trap 'exit 0;' 15
+while [ "$command" != "end" ]; do
+	if [ "$command" = "-h" ] || [ "$command" = "--help" ]; then
+		echo "[serverQuery][HELP] help requested with -h or --help"
+		echo "[serverQuery][HELP] usage: query command [arg1, arg2, ...]"
+		echo "[serverQuery][HELP] "
+		echo "[serverQuery][HELP] special commands:"
+		echo "[serverQuery][HELP] end   - end query"
+		echo "[serverQuery][HELP] reset - clear terminal"
+		echo "[serverQuery][HELP] show  - show last 50 lines of latest.log"
+		echo "[serverQuery][HELP] "
+		echo "[serverQuery][HELP] minecraft server commands:"
+		echo "[serverQuery][HELP] help  - show all minecraft commands"
+	elif [ "$command" = "show" ]; then
+		linesOut=50
+	elif [ "$command" = "reset" ]; then
+		reset
+	else
+		linesBefore="$(getLogLines)"
+		querySend "$command"
+		timeout=5
+		current=1
+		while [ "$current" -lt "$timeout" ] && [ "$(getLogLines)" = "$linesBefore" ]; do
+			usleep 100000 #100ms
+			current=$(( current + 1 ))
+		done
+		if [ "$current" -ge "$timeout" ]; then
+			echo "[serverQuery][ERROR] timeout ${current}/${timeout}"
+		fi
+		linesAfter="$(getLogLines)"
+
+		if [ "$linesBefore" -gt "$linesAfter" ]; then
+			linesOut=20
+		else
+			linesOut=$(( linesAfter - linesBefore ))
+		fi
+	fi
+	
+	tail -n "$linesOut" "$USE_LOG"
+
+    echo "[serverQuery][INFO] insert command, insert end or ctrl+c"
+    read -r command
+done
+
+
+
+
+exit 0
