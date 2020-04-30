0ef1c4f44d79521b85c9530d29df6ff23b424510
Lucas Briese <lbriese@mail.uni-paderborn.de>
2020-04-30 19:29:57 +0200

    ~ fix, corrected server properties settings, added var to ignore it

--- base/8jre/alpine/entrypoint.sh
+++ base/8jre/alpine/entrypoint.sh
@@ -131,8 +131,12 @@ writeServerProperty() {
 	error=$?
 	set -e
 	if [ "1" = "$error" ]; then
-		echo "[entrypoint][WARN] illegal value($value) for $name, fallback to ($default), used regex pattern $pattern"
-		value="$default"
+		if [ "false" = "$IGNORE_PROPERTY_ERRORS" ]; then
+			echo "[entrypoint][WARN] illegal value($value) for $name, fallback to ($default), used regex pattern $pattern"
+			value="$default"
+		else
+			echo "[entrypoint][WARN] illegal value($value) for $name, fallback to ($default) disabled, used regex pattern $pattern"
+		fi
 	fi
 	
 	if [ "$value" = "$default" ]; then
@@ -162,13 +166,13 @@ writeServerProperties() {
 		writeServerProperty "allow-flight" "${allow_flight:?}" "$patternBoolean" "false"
 		writeServerProperty "allow-nether" "${allow_nether:?}" "$patternBoolean" "true"
 		writeServerProperty "broadcast-console-to-ops" "${broadcast_console_to_ops:?}" "$patternBoolean" "true"
-		writeServerProperty "difficulty" "${difficulty:?}" "^[0-3]$" "1"
+		writeServerProperty "difficulty" "${difficulty:?}" "^([0-3]|peaceful|easy|normal|hard)$" "1"
 		writeServerProperty "enable-query" "${enable_query:?}" "$patternBoolean" "false"
 		writeServerProperty "enable-rcon" "${enable_rcon:?}" "$patternBoolean" "false"
 		writeServerProperty "enable-command-block" "${enable_command_block:?}" "$patternBoolean" "false"
 		writeServerProperty "enforce-whitelist" "${enforce_whitelist:?}" "$patternBoolean" "false"
 		writeServerProperty "force-gamemode" "${force_gamemode:?}" "$patternBoolean" "false"
-		writeServerProperty "gamemode" "${gamemode:?}" "^[0-3]$" "0"
+		writeServerProperty "gamemode" "${gamemode:?}" "^([0-3]|survival|creative|adventure|spectator)$" "0"
 		writeServerProperty "generate-structures" "${generate_structures:?}" "$patternBoolean" "true"
 		# shellcheck disable=SC2154
 		writeServerProperty "generator-settings" "${generator_settings}" "^.*$" "" #if user is setting this, he knows what he does
@@ -176,17 +180,17 @@ writeServerProperties() {
 		writeServerProperty "level-name" "${level_name:?}" "^[a-zA-Z0-9]([ a-zA-Z0-9]*[a-zA-Z0-9])?$" "world"
 		# shellcheck disable=SC2154
 		writeServerProperty "level-seed" "${level_seed}" "^.*$" ""
-		writeServerProperty "level-type" "${level_type:?}" "^(DEFAULT|FLAT|LARGEBIOMES|AMPLIFIED|BUFFET)$" "DEFAULT"
+		writeServerProperty "level-type" "${level_type:?}" "^.+$" "DEFAULT" # not matching mod types: (DEFAULT|FLAT|LARGEBIOMES|AMPLIFIED|BUFFET)
 		writeServerProperty "max-build-height" "${max_build_height:?}" "^\d+$" "256"
 		writeServerProperty "max-players" "${max_players:?}" "^[1-9][0-9]*$" "20"
-		writeServerProperty "max-tick-time" "${max_tick_time:?}" "^\d+$" "60000" #yes 0 is allowed
-		writeServerProperty "max-world-size" "${max_world_size:?}" "^\d+$" "29999984"
+		writeServerProperty "max-tick-time" "${max_tick_time:?}" "^(-1|[0-9]+)$" "60000"
+		writeServerProperty "max-world-size" "${max_world_size:?}" "^[0-9]+$" "29999984"
 		# shellcheck disable=SC2154
 		writeServerProperty "motd" "${motd}" "^.*$" "A Minecraft Server"
-		writeServerProperty "network-compression-threshold" "${network_compression_threshold:?}" "^\d+$" "256"
+		writeServerProperty "network-compression-threshold" "${network_compression_threshold:?}" "^(-1|[0-9]+)$" "256"
 		writeServerProperty "online-mode" "${online_mode:?}" "$patternBoolean" "true"
 		writeServerProperty "op-permission-level" "${op_permission_level:?}" "^[1-4]$" "4"
-		writeServerProperty "player-idle-timeout" "${player_idle_timeout:?}" "^\d+$" "0"
+		writeServerProperty "player-idle-timeout" "${player_idle_timeout:?}" "^[0-9]+$" "0"
 		writeServerProperty "prevent-proxy-connections" "${prevent_proxy_connections:?}" "$patternBoolean" "false"
 		writeServerProperty "pvp" "${pvp:?}" "$patternBoolean" "true"
 		writeServerProperty "query.port" "${query_port:?}" "^[1-9][0-9]*$" "25565"
@@ -204,7 +208,7 @@ writeServerProperties() {
 		writeServerProperty "spawn-animals" "${spawn_animals:?}" "$patternBoolean" "true"
 		writeServerProperty "spawn-monsters" "${spawn_monsters:?}" "$patternBoolean" "true"
 		writeServerProperty "spawn-npcs" "${spawn_npcs:?}" "$patternBoolean" "true"
-		writeServerProperty "spawn-protection" "${spawn_protection:?}" "^\d+$" "16"
+		writeServerProperty "spawn-protection" "${spawn_protection:?}" "^[0-9]+$" "16"
 		writeServerProperty "view-distance" "${view_distance:?}" "^([3-9]|1[0-5])$" "10"
 		writeServerProperty "white-list" "${white_list:?}" "$patternBoolean" "false"
 	else
